b637c5fcc3e293875116080d8ab6ccfb31c443e7
wyouflf <wyouflf@gmail.com>
2013-08-02 10:20:32 +0800

    update MIME to public

--- src/com/lidroid/xutils/http/client/multipart/MIME.java
+++ src/com/lidroid/xutils/http/client/multipart/MIME.java
@@ -22,7 +22,7 @@ import java.nio.charset.Charset;
 /**
  * @since 4.0
  */
-class MIME {
+public class MIME {
 
     public static final String CONTENT_TYPE = "Content-Type";
     public static final String CONTENT_TRANSFER_ENC = "Content-Transfer-Encoding";
