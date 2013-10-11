969430a17e0bde1fae0c3c33816e9b982431bff0
wyouflf <wyouflf@gmail.com>
2013-10-11 23:14:33 +0800

    update BitmapUtils

--- library/src/com/lidroid/xutils/BitmapUtils.java
+++ library/src/com/lidroid/xutils/BitmapUtils.java
@@ -16,9 +16,7 @@
 package com.lidroid.xutils;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.ColorFilter;
+import android.graphics.*;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.text.TextUtils;
@@ -337,6 +335,126 @@ public class BitmapUtils {
         @Override
         public int getOpacity() {
             return baseDrawable.getOpacity();
+        }
+
+        @Override
+        public void setBounds(int left, int top, int right, int bottom) {
+            baseDrawable.setBounds(left, top, right, bottom);
+        }
+
+        @Override
+        public void setBounds(Rect bounds) {
+            baseDrawable.setBounds(bounds);
+        }
+
+        @Override
+        public void setChangingConfigurations(int configs) {
+            baseDrawable.setChangingConfigurations(configs);
+        }
+
+        @Override
+        public int getChangingConfigurations() {
+            return baseDrawable.getChangingConfigurations();
+        }
+
+        @Override
+        public void setDither(boolean dither) {
+            baseDrawable.setDither(dither);
+        }
+
+        @Override
+        public void setFilterBitmap(boolean filter) {
+            baseDrawable.setFilterBitmap(filter);
+        }
+
+        @Override
+        public void invalidateSelf() {
+            baseDrawable.invalidateSelf();
+        }
+
+        @Override
+        public void scheduleSelf(Runnable what, long when) {
+            baseDrawable.scheduleSelf(what, when);
+        }
+
+        @Override
+        public void unscheduleSelf(Runnable what) {
+            baseDrawable.unscheduleSelf(what);
+        }
+
+        @Override
+        public void setColorFilter(int color, PorterDuff.Mode mode) {
+            baseDrawable.setColorFilter(color, mode);
+        }
+
+        @Override
+        public void clearColorFilter() {
+            baseDrawable.clearColorFilter();
+        }
+
+        @Override
+        public boolean isStateful() {
+            return baseDrawable.isStateful();
+        }
+
+        @Override
+        public boolean setState(int[] stateSet) {
+            return baseDrawable.setState(stateSet);
+        }
+
+        @Override
+        public int[] getState() {
+            return baseDrawable.getState();
+        }
+
+        @Override
+        public Drawable getCurrent() {
+            return baseDrawable.getCurrent();
+        }
+
+        @Override
+        public boolean setVisible(boolean visible, boolean restart) {
+            return baseDrawable.setVisible(visible, restart);
+        }
+
+        @Override
+        public Region getTransparentRegion() {
+            return baseDrawable.getTransparentRegion();
+        }
+
+        @Override
+        public int getIntrinsicWidth() {
+            return baseDrawable.getIntrinsicWidth();
+        }
+
+        @Override
+        public int getIntrinsicHeight() {
+            return baseDrawable.getIntrinsicHeight();
+        }
+
+        @Override
+        public int getMinimumWidth() {
+            return baseDrawable.getMinimumWidth();
+        }
+
+        @Override
+        public int getMinimumHeight() {
+            return baseDrawable.getMinimumHeight();
+        }
+
+        @Override
+        public boolean getPadding(Rect padding) {
+            return baseDrawable.getPadding(padding);
+        }
+
+        @Override
+        public Drawable mutate() {
+            return baseDrawable.mutate();
+        }
+
+        @Override
+        public ConstantState getConstantState() {
+            return baseDrawable.getConstantState();
         }
     }
 
